adef9ad7160f543311faea7d51794b9bd6bd2611
ariadiprana <ari.adiprana@gmail.com>
2017-06-15 13:41:32 +0700

    release

--- app/routes.js
+++ app/routes.js
@@ -16,6 +16,29 @@ module.exports = function(app, passport) {
 	app.get('/about_us', function(req, res) { res.render('about_us.ejs'); });
 	app.get('/services', function(req, res) { res.render('services.ejs'); });
 
+	// SALES
+	app.get('/finansial/kredit-jaminan-bpkb', function(req, res) { res.render('finansial-kredit-jaminan-bpkb.ejs'); });
+	app.get('/finansial/kredit-jaminan-sertifikat', function(req, res) { res.render('finansial-kredit-jaminan-sertifikat.ejs'); });
+	
+	app.get('/asuransi/kendaraan', function(req, res) { res.render('asuransi-kendaraan.ejs'); });
+	app.get('/asuransi/properti', function(req, res) { res.render('asuransi-properti.ejs'); });
+	app.get('/asuransi/perjalanan', function(req, res) { res.render('asuransi-perjalanan.ejs'); });
+	
+	app.get('/kartu-kredit/cash-back', function(req, res) { res.render('kartu-kredit-cash-back.ejs'); });
+	app.get('/kartu-kredit/travelling', function(req, res) { res.render('kartu-kredit-travelling.ejs'); });
+	app.get('/kartu-kredit/promosi', function(req, res) { res.render('kartu-kredit-promosi.ejs'); });
+	app.get('/kartu-kredit/reward', function(req, res) { res.render('kartu-kredit-reward.ejs'); });
+	app.get('/kartu-kredit/shopping', function(req, res) { res.render('kartu-kredit-shopping.ejs'); });
+
+	// TRAINING
+	app.get('/training/professional-selling-skill', function(req, res) { res.render('training-professional-selling-skill.ejs'); });
+	app.get('/training/sales-leadership', function(req, res) { res.render('training-sales-leadership.ejs'); });
+	app.get('/training/customer-relationship-management', function(req, res) { res.render('training-customer-relationship-management.ejs'); });
+	app.get('/training/collection-skill', function(req, res) { res.render('training-collection-skill.ejs'); });
+	app.get('/training/managing-for-growth', function(req, res) { res.render('training-managing-for-growth.ejs'); });
+	app.get('/training/professional-development-and-team-building', function(req, res) { res.render('training-professional-development-and-team-building.ejs'); });
+	app.get('/training/learning-and-development', function(req, res) { res.render('learning-and-development.ejs'); });
+	
 }
 
 // route middleware to make sure
