0afbd8fb4cdc1ac86521eda7812c24ec49dd92b2
gerardus ari adiprana <gerardus.adiprana@metraplasa.co.id>
2017-07-09 21:20:03 +0700

    release

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,14 @@
+var nodemailer = require("nodemailer");
+var smtpTransport = nodemailer.createTransport({
+		service: "gmail",
+		host: "smtp.gmail.com",
+		auth: {
+				user: "salesku.id@gmail.com",
+				pass: "Ad1pr4n4"
+		}
+})
+
+
 // app/routes.js
 module.exports = function(app, passport) {
 
@@ -11,6 +22,188 @@ module.exports = function(app, passport) {
 	// =====================================
 	// home screen
 	// =====================================
+
+	app.post('/email-sales-support', function(req, res) {
+		console.log(req.body)
+		var namaForm = req.body.fields.namaForm
+		var namaDepan = req.body.fields.namaDepan
+		var namaTengah = req.body.fields.namaTengah
+		var namaBelakang = req.body.fields.namaBelakang
+		var jk = req.body.fields.jk
+		var email = req.body.fields.email
+		var noTelpRumah = req.body.fields.noTelpRumah
+		var noTelpHp = req.body.fields.noTelpHp
+		var kotaDomisili = req.body.fields.kotaDomisili
+		var namaPerusahaan = req.body.fields.namaPerusahaan
+		var sektorUsaha = req.body.fields.sektorUsaha
+		var jabatan = req.body.fields.jabatan
+		var emailKantor = req.body.fields.emailKantor
+		var telKantor = req.body.fields.telKantor
+		var alamatKantor = req.body.fields.alamatKantor
+
+		var subjectTitle = 'Informasi Pengunjung Salesku - '+namaForm
+
+		var mailOptions={
+		   to : 'ari.adiprana@gmail.com',
+		   subject : subjectTitle,
+		   text : `Berikut informasi pengunjung salesku untuk ${namaForm}\n
+			 Nama Depan  	    : ${namaDepan} \n
+			 Nama Tengah 	    : ${namaTengah} \n
+			 Nama Belakang    : ${namaBelakang} \n
+			 Jenis Kelamin    : ${jk} \n
+			 Email 			      : ${email} \n
+			 No Telp Rumah    : ${noTelpRumah} \n
+			 No Handphone     : ${noTelpHp} \n
+			 Kota Domisili    : ${kotaDomisili} \n
+			 Nama Perusahaan  : ${namaPerusahaan} \n
+			 Sektor Usaha     : ${sektorUsaha} \n
+			 Jabatan          : ${jabatan} \n
+			 Email Kantor     : ${emailKantor} \n
+			 Telpon Kantor    : ${telKantor} \n
+			 Alamat Kantor    : ${alamatKantor} 
+			 `
+		}
+		console.log(mailOptions);
+		smtpTransport.sendMail(mailOptions, function(error, response){
+			if(error){
+				console.log(error);
+				res.end("error");
+			}else{
+				console.log("Message sent: " + response.message);
+				res.end("sent");
+			}
+		});
+	})
+
+	app.post('/email-finansial', function(req, res) {
+		console.log(req.body)
+		var namaForm = req.body.fields.namaForm
+		var namaDepan = req.body.fields.namaDepan
+		var namaTengah = req.body.fields.namaTengah
+		var namaBelakang = req.body.fields.namaBelakang
+		var jk = req.body.fields.jk
+		var email = req.body.fields.email
+		var noTelpRumah = req.body.fields.noTelpRumah
+		var noTelpHp = req.body.fields.noTelpHp
+		var kotaDomisili = req.body.fields.kotaDomisili
+		var statusPekerjaan = req.body.fields.statusPekerjaan
+		var penghasilanPekerjaan = req.body.fields.penghasilanPekerjaan
+		var subjectTitle = 'Informasi Pengunjung Salesku - '+namaForm
+
+		var mailOptions={
+		   to : 'ari.adiprana@gmail.com',
+		   subject : subjectTitle,
+		   text : `Berikut informasi pengunjung salesku untuk ${namaForm}\n
+			 Nama Depan  	    : ${namaDepan} \n
+			 Nama Tengah 	    : ${namaTengah} \n
+			 Nama Belakang    : ${namaBelakang} \n
+			 Jenis Kelamin    : ${jk} \n
+			 Email 			      : ${email} \n
+			 No Telp Rumah    : ${noTelpRumah} \n
+			 No Handphone     : ${noTelpHp} \n
+			 Kota Domisili    : ${kotaDomisili} \n
+			 Status Pekerjaan : ${statusPekerjaan} \n
+			 Penghasilan      : ${penghasilanPekerjaan}
+			 `
+		}
+		console.log(mailOptions);
+		smtpTransport.sendMail(mailOptions, function(error, response){
+			if(error){
+				console.log(error);
+				res.end("error");
+			}else{
+				console.log("Message sent: " + response.message);
+				res.end("sent");
+			}
+		});
+	})
+
+	app.post('/email-kartu-kredit', function(req, res) {
+		console.log(req.body)
+		var namaForm = req.body.fields.namaForm
+		var namaDepan = req.body.fields.namaDepan
+		var namaTengah = req.body.fields.namaTengah
+		var namaBelakang = req.body.fields.namaBelakang
+		var jk = req.body.fields.jk
+		var email = req.body.fields.email
+		var noTelpRumah = req.body.fields.noTelpRumah
+		var noTelpHp = req.body.fields.noTelpHp
+		var kotaDomisili = req.body.fields.kotaDomisili
+		var statusPekerjaan = req.body.fields.statusPekerjaan
+		var penghasilanPekerjaan = req.body.fields.penghasilanPekerjaan
+		var subjectTitle = 'Informasi Pengunjung Salesku - '+namaForm
+
+		var mailOptions={
+		   to : 'ari.adiprana@gmail.com',
+		   subject : subjectTitle,
+		   text : `Berikut informasi pengunjung salesku untuk ${namaForm}\n
+			 Nama Depan  	    : ${namaDepan} \n
+			 Nama Tengah 	    : ${namaTengah} \n
+			 Nama Belakang    : ${namaBelakang} \n
+			 Jenis Kelamin    : ${jk} \n
+			 Email 			      : ${email} \n
+			 No Telp Rumah    : ${noTelpRumah} \n
+			 No Handphone     : ${noTelpHp} \n
+			 Kota Domisili    : ${kotaDomisili} \n
+			 Status Pekerjaan : ${statusPekerjaan} \n
+			 Penghasilan      : ${penghasilanPekerjaan}
+			 `
+		}
+		console.log(mailOptions);
+		smtpTransport.sendMail(mailOptions, function(error, response){
+			if(error){
+				console.log(error);
+				res.end("error");
+			}else{
+				console.log("Message sent: " + response.message);
+				res.end("sent");
+			}
+		});
+	})
+
+	app.post('/email-asuransi', function(req, res) {
+		console.log(req.body)
+		var namaForm = req.body.fields.namaForm
+		var namaDepan = req.body.fields.namaDepan
+		var namaTengah = req.body.fields.namaTengah
+		var namaBelakang = req.body.fields.namaBelakang
+		var jk = req.body.fields.jk
+		var email = req.body.fields.email
+		var noTelpRumah = req.body.fields.noTelpRumah
+		var noTelpHp = req.body.fields.noTelpHp
+		var kotaDomisili = req.body.fields.kotaDomisili
+		var statusPekerjaan = req.body.fields.statusPekerjaan
+		var penghasilanPekerjaan = req.body.fields.penghasilanPekerjaan
+		var subjectTitle = 'Informasi Pengunjung Salesku - '+namaForm
+
+		var mailOptions={
+		   to : 'ari.adiprana@gmail.com',
+		   subject : subjectTitle,
+		   text : `Berikut informasi pengunjung salesku untuk ${namaForm}\n
+			 Nama Depan  	    : ${namaDepan} \n
+			 Nama Tengah 	    : ${namaTengah} \n
+			 Nama Belakang    : ${namaBelakang} \n
+			 Jenis Kelamin    : ${jk} \n
+			 Email 			      : ${email} \n
+			 No Telp Rumah    : ${noTelpRumah} \n
+			 No Handphone     : ${noTelpHp} \n
+			 Kota Domisili    : ${kotaDomisili} \n
+			 Status Pekerjaan : ${statusPekerjaan} \n
+			 Penghasilan      : ${penghasilanPekerjaan}
+			 `
+		}
+		console.log(mailOptions);
+		smtpTransport.sendMail(mailOptions, function(error, response){
+			if(error){
+				console.log(error);
+				res.end("error");
+			}else{
+				console.log("Message sent: " + response.message);
+				res.end("sent");
+			}
+		});
+	})
+
 	app.get('/', function(req, res) { res.render('main.ejs'); });
 	app.get('/contact', function(req, res) { res.render('contact.ejs'); });
 	app.get('/about_us', function(req, res) { res.render('about_us.ejs'); });
@@ -21,7 +214,7 @@ module.exports = function(app, passport) {
 	app.get('/finansial/kredit-pemilikan-rumah', function(req, res) { res.render('finansial-kredit-pemilikan-rumah.ejs'); });
 	app.get('/finansial/kredit-agunan-bpkb', function(req, res) { res.render('finansial-kredit-agunan-bpkb.ejs'); });
 	app.get('/finansial/kredit-agunan-sertifikat', function(req, res) { res.render('finansial-kredit-agunan-sertifikat.ejs'); });
-	
+
 	app.get('/asuransi/mobil', function(req, res) { res.render('asuransi-mobil.ejs'); });
 	app.get('/asuransi/motor', function(req, res) { res.render('asuransi-motor.ejs'); });
 	app.get('/asuransi/sepeda', function(req, res) { res.render('asuransi-sepeda.ejs'); });
@@ -37,9 +230,9 @@ module.exports = function(app, passport) {
 	app.get('/asuransi/marine-cargo', function(req, res) { res.render('asuransi-marine-cargo.ejs'); });
 	app.get('/asuransi/term-life', function(req, res) { res.render('asuransi-term-life.ejs'); });
 	app.get('/asuransi/unit-link', function(req, res) { res.render('asuransi-unit-link.ejs'); });
-	app.get('/asuransi/dana-pensiun', function(req, res) { res.render('dana-pensiun.ejs'); });
-	 
-	
+	app.get('/asuransi/dana-pensiun', function(req, res) { res.render('asuransi-dana-pensiun.ejs'); });
+
+
 	app.get('/kartu-kredit/promo', function(req, res) { res.render('kartu-kredit-promo.ejs'); });
 	app.get('/kartu-kredit/reward', function(req, res) { res.render('kartu-kredit-reward.ejs'); });
 	app.get('/kartu-kredit/cashback', function(req, res) { res.render('kartu-kredit-cashback.ejs'); });
@@ -65,7 +258,7 @@ module.exports = function(app, passport) {
 	app.get('/training/collection-skill', function(req, res) { res.render('training-collection-skill.ejs'); });
 	app.get('/training/managing-for-growth', function(req, res) { res.render('training-managing-for-growth.ejs'); });
 	app.get('/training/professional-development-and-team-building', function(req, res) { res.render('training-professional-development-and-team-building.ejs'); });
-	
+
 	// SALES SUPPORT
 	app.get('/sales-support/online-offline', function(req, res) { res.render('sales-support-online-offline.ejs'); });
 	app.get('/sales-support/multi-product', function(req, res) { res.render('sales-support-multi-product.ejs'); });
@@ -73,7 +266,7 @@ module.exports = function(app, passport) {
 	app.get('/sales-support/sales-empowering', function(req, res) { res.render('sales-support-sales-empowering.ejs'); });
 	// Initial
 	//app.get('', function(req, res) { res.render('.ejs'); });
-	
+
 }
 
 // route middleware to make sure
